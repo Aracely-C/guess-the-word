eb35a87b0b3aa10c39c9a99a5463a13ec30a0e60
aracely-c <aracely-7@hotmail.com>
2023-04-06 11:57:30 -0700

    Make uppercase letters and addletters to replace symbol

--- js/script.js
+++ js/script.js
@@ -58,6 +58,26 @@ guessedLetters.innerHTML = "";
 for (const letter of guessedLetters) {
     const li = document.createElement("li");
     li.innerText = letter;
-    guessedLetters.append(li);
+    lettersGuessed.append(li);
 }
+};
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+      if (guessedLetters.includes(letter)) {
+        revealWord.push(letter.toUpperCase());
+      } else {
+        revealWord.push("●");
+      }
+    }
+    //console.log(revealWord);
+    wordInProgress.innerText = revealWord.join("");
+  };
+const checkIfWon = function() {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerText = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
 };
